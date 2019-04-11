6beb0ba33b01f61c982cd992b232e4e5b087ca9e
Excelsus <elijahko4@gmail.com>
2019-04-11 17:34:41 +0900

    Re

--- hand.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-// hand is double
-var hand = [1,2,3,4]
-for i in 0...3 {
-    hand[i] = hand[i] * 2 - 1
-}
-print(hand)
--- ice.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-// Ice is very cold
-var door = ["Ice ", "is ", "very ", "cold"]
-var st = ""
-for i in 0...3{
-    st = st + door[i]
-}
-print(st)
